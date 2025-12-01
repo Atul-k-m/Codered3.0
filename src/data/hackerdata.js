15fec2f657ec7176a015498db408f4e1020e8d74
Atul Kumar Mishra <atulkm189@gmail.com>
2025-12-02 00:51:10 +0530

    push

--- src/data/hackerdata.js
+++ src/data/hackerdata.js
@@ -98,7 +98,49 @@ const hackersData = [
     { id: 10, name: "PRIME", lead: "ASHWINI HADAGALI", quote: "", category: "H5", status: "Waiting", domain: "Hardware" },
 
     // H6
-    { id: 11, name: "STASH Monarchs", lead: "Tharun Susri B S", quote: "", category: "H6", status: "Waiting", domain: "Hardware" }
+    { id: 11, name: "STASH Monarchs", lead: "Tharun Susri B S", quote: "", category: "H6", status: "Waiting", domain: "Hardware" },
+     { id: 1, name: "The Quadrent", lead: "Manjula Rani", quote: "", category: "U2", status: "Shortlisted", domain: "Unisys" },
+    { id: 2, name: "No code paradigm", lead: "Vinyas G M", quote: "", category: "U2", status: "Shortlisted", domain: "Unisys" },
+
+    // U3
+    { id: 3, name: "Forbidden Phishers", lead: "Shashank Pandey", quote: "", category: "U3", status: "Shortlisted", domain: "Unisys" },
+    { id: 4, name: "INNOVCAMP", lead: "Nithin Naik R", quote: "", category: "U3", status: "Shortlisted", domain: "Unisys" },
+    { id: 5, name: "CODEBREAKER5", lead: "Sidhartha NS", quote: "", category: "U3", status: "Shortlisted", domain: "Unisys" },
+
+    // U4
+    { id: 6, name: "GARUDA", lead: "Srijan U", quote: "", category: "U4", status: "Shortlisted", domain: "Unisys" },
+    { id: 7, name: "Redbyte", lead: "Mansi Kalgudi", quote: "", category: "U4", status: "Shortlisted", domain: "Unisys" },
+    { id: 8, name: "Run Time Terror", lead: "Pratyush Visu", quote: "", category: "U4", status: "Shortlisted", domain: "Unisys" },
+
+    // U5
+    { id: 9, name: "Mjolnir", lead: "Dheeksha N", quote: "", category: "U5", status: "Shortlisted", domain: "Unisys" },
+    { id: 10, name: "MSN", lead: "Nithin J", quote: "", category: "U5", status: "Shortlisted", domain: "Unisys" },
+    { id: 11, name: "Caffeine Crew", lead: "Thejaswini V Bhat", quote: "", category: "U5", status: "Shortlisted", domain: "Unisys" },
+    { id: 12, name: "SlothX", lead: "Nuvvula Geethika", quote: "", category: "U5", status: "Shortlisted", domain: "Unisys" },
+
+    // U6
+    { id: 13, name: "Tesla_Curie", lead: "Abhiragini K", quote: "", category: "U6", status: "Shortlisted", domain: "Unisys" },
+    { id: 14, name: "Team 269", lead: "Keerthi Narayan M V", quote: "", category: "U6", status: "Shortlisted", domain: "Unisys" },
+
+    // U7
+    { id: 15, name: "Infinity Devs", lead: "Sumit Srivastava", quote: "", category: "U7", status: "Shortlisted", domain: "Unisys" },
+    { id: 16, name: "Prototype Pirates", lead: "Nipun Gupta", quote: "", category: "U7", status: "Shortlisted", domain: "Unisys" },
+    { id: 17, name: "Tenet", lead: "Satvika Prashanth Hiremath", quote: "", category: "U7", status: "Shortlisted", domain: "Unisys" },
+
+    // U8
+    { id: 18, name: "Team 0/1", lead: "Sunder Gandhi Ningappa", quote: "", category: "U8", status: "Shortlisted", domain: "Unisys" },
+    { id: 19, name: "LOLgorithms", lead: "Sushruth Kumar", quote: "", category: "U8", status: "Shortlisted", domain: "Unisys" },
+    { id: 20, name: "Unified Cargo Exchange", lead: "V Nanda Kishore Naik", quote: "", category: "U8", status: "Shortlisted", domain: "Unisys" },
+    { id: 100, name: "4Direction", lead: "Aman", quote: "", category: "U2", status: "Waiting", domain: "Unisys" },
+    { id: 101, name: "Team Algorithm", lead: "Mustafa Joyabali Vora", quote: "", category: "U3", status: "Waiting", domain: "Unisys" },
+    { id: 102, name: "Tech Dominators", lead: "Akhil", quote: "", category: "U4", status: "Waiting", domain: "Unisys" },
+    { id: 103, name: "The 8th Question", lead: "Marmik Jain", quote: "", category: "U5", status: "Waiting", domain: "Unisys" },
+    { id: 104, name: "tech titans", lead: "Aditya Kumar Bangre", quote: "", category: "U6", status: "Waiting", domain: "Unisys" },
+    { id: 105, name: "Byte_Me", lead: "Shreyas Santosh Naik", quote: "", category: "U7", status: "Waiting", domain: "Unisys" },
+    { id: 106, name: "Minus Zero", lead: "Kishan MN", quote: "", category: "U8", status: "Waiting", domain: "Unisys" }
+
+    
+
 
 ];
 
